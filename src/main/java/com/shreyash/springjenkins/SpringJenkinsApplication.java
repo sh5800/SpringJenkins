2474e02b2d06cb5d5867a18d5cb04ef8ff32c0cb
sh5800 <shreyaskashyap05@gmail.com>
2023-01-31 18:52:40 +0530

    intentionally added an error

--- src/main/java/com/shreyash/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/shreyash/springjenkins/SpringJenkinsApplication.java
@@ -12,7 +12,7 @@ public class SpringJenkinsApplication {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 	@PostConstruct
-	public void init(){
+	public init(){
 		logger.info("Application started .....");
 	}
 
